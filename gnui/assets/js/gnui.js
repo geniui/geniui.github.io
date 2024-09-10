c8fe4d270a186270a3384202b50eb133b69fe1c9
geniui <chacha@genians.com>
2024-09-10 16:10:40 +0900

    update docs GNUI-CT64-C-1104-1.0.0.0910

--- gnui/assets/js/gnui.js
+++ gnui/assets/js/gnui.js
@@ -14691,6 +14691,124 @@
 	    }
 	}
 
+	const base64 = {
+	    // private property
+	    _keyStr: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
+	    // public method for encoding
+	    encode: function (input) {
+	        let output = '';
+	        let chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+	        let i = 0;
+	        input = base64._utf8_encode(input);
+	        while (i < input.length) {
+	            chr1 = input.charCodeAt(i++);
+	            chr2 = input.charCodeAt(i++);
+	            chr3 = input.charCodeAt(i++);
+	            enc1 = chr1 >> 2;
+	            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+	            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+	            enc4 = chr3 & 63;
+	            if (isNaN(chr2)) {
+	                enc3 = enc4 = 64;
+	            }
+	            else if (isNaN(chr3)) {
+	                enc4 = 64;
+	            }
+	            output = output + this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) + this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+	        }
+	        return output;
+	    },
+	    // public method for decoding
+	    decode: function (input) {
+	        let output = '';
+	        let chr1, chr2, chr3;
+	        let enc1, enc2, enc3, enc4;
+	        let i = 0;
+	        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, '');
+	        while (i < input.length) {
+	            enc1 = this._keyStr.indexOf(input.charAt(i++));
+	            enc2 = this._keyStr.indexOf(input.charAt(i++));
+	            enc3 = this._keyStr.indexOf(input.charAt(i++));
+	            enc4 = this._keyStr.indexOf(input.charAt(i++));
+	            chr1 = (enc1 << 2) | (enc2 >> 4);
+	            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+	            chr3 = ((enc3 & 3) << 6) | enc4;
+	            output = output + String.fromCharCode(chr1);
+	            if (enc3 != 64) {
+	                output = output + String.fromCharCode(chr2);
+	            }
+	            if (enc4 != 64) {
+	                output = output + String.fromCharCode(chr3);
+	            }
+	        }
+	        output = base64._utf8_decode(output);
+	        return output;
+	    },
+	    // private method for UTF-8 encoding
+	    _utf8_encode: function (string) {
+	        string = string.replace(/\r\n/g, '\n');
+	        let utftext = '';
+	        for (let n = 0; n < string.length; n++) {
+	            const c = string.charCodeAt(n);
+	            if (c < 128) {
+	                utftext += String.fromCharCode(c);
+	            }
+	            else if (c > 127 && c < 2048) {
+	                utftext += String.fromCharCode((c >> 6) | 192);
+	                utftext += String.fromCharCode((c & 63) | 128);
+	            }
+	            else {
+	                utftext += String.fromCharCode((c >> 12) | 224);
+	                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+	                utftext += String.fromCharCode((c & 63) | 128);
+	            }
+	        }
+	        return utftext;
+	    },
+	    // private method for UTF-8 decoding
+	    _utf8_decode: function (utftext) {
+	        let string = '';
+	        let i = 0;
+	        let c2, c3;
+	        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+	        let c = (c2 = 0);
+	        while (i < utftext.length) {
+	            c = utftext.charCodeAt(i);
+	            if (c < 128) {
+	                string += String.fromCharCode(c);
+	                i++;
+	            }
+	            else if (c > 191 && c < 224) {
+	                c2 = utftext.charCodeAt(i + 1);
+	                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+	                i += 2;
+	            }
+	            else {
+	                c2 = utftext.charCodeAt(i + 1);
+	                c3 = utftext.charCodeAt(i + 2);
+	                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+	                i += 3;
+	            }
+	        }
+	        return string;
+	    },
+	    base64UrlEncode: function (val) {
+	        const r = base64.encode(val).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
+	        return r;
+	    },
+	    base64UrlDecode: function (base64url) {
+	        let input = base64url.replace(/-/g, '+').replace(/_/g, '/');
+	        let diff = input.length % 4;
+	        if (!diff) {
+	            while (diff) {
+	                input += '=';
+	                diff--;
+	            }
+	        }
+	        return base64.decode(input);
+	    }
+	};
+
 	function addClass(element, ...args) {
 	    apply(element, args, 'add');
 	}
@@ -15796,6 +15914,7 @@
 		append: append,
 		assign: assign$1,
 		attr: attr,
+		base64: base64,
 		baseSlice: baseSlice,
 		before: before,
 		byteSize: byteSize,
